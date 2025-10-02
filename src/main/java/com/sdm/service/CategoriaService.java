9acc84f6ec798ae618238b9b899937162104389e
Gabriel <gmdf10@gmail.com>
2025-10-02 17:55:35 -0300

    feat(service): implementar CategoriaService com métodos salvar, atualizar, deletar, listar e buscarPorId

--- src/main/java/com/sdm/service/CategoriaService.java
+++ src/main/java/com/sdm/service/CategoriaService.java
@@ -1,4 +1,29 @@
 package com.sdm.service;
 
+import com.sdm.dao.CategoriaDAO;
+import com.sdm.model.Categoria;
+import java.util.List;
+
 public class CategoriaService {
+    private CategoriaDAO dao = new CategoriaDAO();
+
+    public void salvar(Categoria categoria) {
+        dao.salvar(categoria);
+    }
+
+    public void atualizar(Categoria categoria) {
+        dao.atualizar(categoria);
+    }
+
+    public void deletar(int id) {
+        dao.deletar(id);
+    }
+
+    public List<Categoria> listar() {
+        return dao.listar();
+    }
+
+    public Categoria buscarPorId(int id) {
+        return dao.buscarPorId(id);
+    }
 }
