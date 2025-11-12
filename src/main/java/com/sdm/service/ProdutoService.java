1057ebecd905ca13ff1783ed8c59f562b156b731
Arthur <akposselt@gmail.com>
2025-11-12 15:28:32 -0300

    feat: adicionando o método inserir na classe produto service.

--- src/main/java/com/sdm/service/ProdutoService.java
+++ src/main/java/com/sdm/service/ProdutoService.java
@@ -1,9 +1,14 @@
 package com.sdm.service;
 
 import com.sdm.dao.ProdutoDAO;
+import com.sdm.model.Produto;
 
 public class ProdutoService {
     private ProdutoDAO produtoDAO = new ProdutoDAO();
+    
+    public void inserir(Produto p) {
+        produtoDAO.inserir(p);
+    }
 
     /*
     Método para reajustar o preço dos produtos em um determinado percentual
