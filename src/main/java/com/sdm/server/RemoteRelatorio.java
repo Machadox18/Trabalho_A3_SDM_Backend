6430ba9ee87e151d3fcd37d7a80201ad41214c3d
Gabriel <gmdf10@gmail.com>
2025-11-14 21:03:59 -0300

    feat(RemoteRelatorio): adicionar novos métodos remotos para relatórios de estoque

---
+++ src/main/java/com/sdm/server/RemoteRelatorio.java
@@ -0,0 +1,27 @@
+package com.sdm.server;
+
+import com.sdm.model.Produto;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
+
+public interface RemoteRelatorio extends Remote {
+
+    List<Produto> produtosAbaixoMinimo() throws RemoteException;
+
+    List<Produto> produtosAcimaMaximo() throws RemoteException;
+
+    /**
+     * Retorna lista com estatísticas de movimentação (ex.: nome, totalSaidas, totalEntradas).
+     */
+    List<Produto> relatorioMovimentacoes() throws RemoteException;
+
+    /**
+     * Retorna um mapa com:
+     *  - "produtos" -> List<Produto> (cada produto com valores necessários)
+     *  - "valor_total_estoque" -> Double
+     */
+    List<Produto> relatorioBalanco() throws RemoteException;
+}
