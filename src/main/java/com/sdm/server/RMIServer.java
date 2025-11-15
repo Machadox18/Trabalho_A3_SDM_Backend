7900cd2f9a6cb224c003c6b868415a2da4db1170
Gabriel <gmdf10@gmail.com>
2025-11-14 21:08:05 -0300

    feat(RMIServer): adicionar registro do RelatorioService no RMI Registry

--- src/main/java/com/sdm/server/RMIServer.java
+++ src/main/java/com/sdm/server/RMIServer.java
@@ -17,16 +17,19 @@ public class RMIServer {
             RemoteProduto produtoService = new RemoteProdutoImpl();
             RemoteCategoria categoriaService = new RemoteCategoriaImpl();
             RemoteMovimentacao movimentacaoService = new RemoteMovimentacaoImpl();
+            RemoteRelatorio relatorioService = new RemoteRelatorioImpl();
 
             registry.rebind("ProdutoService", produtoService);
             registry.rebind("CategoriaService", categoriaService);
             registry.rebind("MovimentacaoService", movimentacaoService);
+            registry.rebind("RelatorioService", new RemoteRelatorioImpl());
 
             System.out.println("Servidor RMI iniciado na porta " + port);
             System.out.println("Serviços registrados:");
             System.out.println("   ProdutoService");
             System.out.println("   CategoriaService");
             System.out.println("   MovimentacaoService");
+            System.out.println("   RelatorioService");
 
         } catch (Exception e) {
             System.err.println("Erro ao iniciar o servidor RMI:");
