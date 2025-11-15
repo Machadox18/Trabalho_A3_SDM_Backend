8d6a12768a7a2adef18e1dd08d52ff1f24b16e61
Gabriel <gmdf10@gmail.com>
2025-11-14 21:04:40 -0300

    feat(RemoteRelatorioImpl): implementar métodos remotos de relatórios conectando ao RelatorioService

---
+++ src/main/java/com/sdm/server/RemoteRelatorioImpl.java
@@ -0,0 +1,37 @@
+package com.sdm.server;
+
+import com.sdm.model.Produto;
+import com.sdm.service.RelatorioService;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+public class RemoteRelatorioImpl extends UnicastRemoteObject implements RemoteRelatorio{
+    private final RelatorioService relatorioService;
+
+    public RemoteRelatorioImpl() throws RemoteException {
+        super();
+        this.relatorioService = new RelatorioService();
+    }
+
+    @Override
+    public List<Produto> produtosAbaixoMinimo() throws RemoteException {
+        return relatorioService.listarAbaixoMinimo();
+    }
+
+    @Override
+    public List<Produto> produtosAcimaMaximo() throws RemoteException {
+        return relatorioService.produtosAcimaMaximo();
+    }
+
+    @Override
+    public List<Produto> relatorioMovimentacoes() throws RemoteException {
+        return relatorioService.listarMovimentacaoProduto();
+    }
+
+    @Override
+    public List<Produto> relatorioBalanco() throws RemoteException {
+        return relatorioService.listarBalancoEstoque();
+    }
+}
